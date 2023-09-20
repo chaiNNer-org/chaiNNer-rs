df9696a10840d4aed5ecf3eb3c284eb558a744a6
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-09-20 21:41:30 +0200

    Removed old test functions

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -4,16 +4,12 @@ mod dither;
 mod macros;
 mod regex;
 
-use glam::Vec4;
-use image_core::{Image, NDimImage, Size};
-use image_ops::{
-    fill_alpha::{fill_alpha, FillMode},
-    scale::nearest_neighbor,
-};
+use image_core::{Image, NDimImage};
+use image_ops::fill_alpha::{fill_alpha, FillMode};
 use numpy::{IntoPyArray, PyArray3, PyReadonlyArrayDyn};
 use pyo3::{exceptions::PyValueError, prelude::*};
 
-use crate::convert::{IntoNumpy, IntoPy};
+use crate::convert::IntoNumpy;
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -31,31 +27,6 @@ fn chainner_ext(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(dither::error_diffusion_dither))?;
     m.add_wrapped(wrap_pyfunction!(dither::ordered_dither))?;
     m.add_wrapped(wrap_pyfunction!(dither::riemersma_dither))?;
-
-    /// Test function
-    #[pyfn(m)]
-    fn test_invert<'py>(
-        py: Python<'py>,
-        img: PyReadonlyArrayDyn<f32>,
-    ) -> PyResult<&'py PyArray3<f32>> {
-        let img = load_image!(img);
-        // let img = img.from_numpy().unwrap();
-        let result = py.allow_threads(move || nearest_neighbor::<Vec4>(&img, img.size().scale(4.)));
-        let result = result.into_numpy().into_pyarray(py);
-        Ok(result)
-    }
-
-    /// Test function
-    #[pyfn(m)]
-    fn test_rainbow(py: Python<'_>) -> PyResult<&PyArray3<f32>> {
-        let result = py.allow_threads(move || {
-            Image::from_fn(Size::new(256, 256), |x, y| {
-                [x as f32 / 255., y as f32 / 255., 0.]
-            })
-        });
-        let result = result.into_py(py);
-        Ok(result)
-    }
 
     /// Fill the transparent pixels in the given image with nearby colors.
     #[pyfn(m)]
